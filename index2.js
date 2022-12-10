fe873912b81fb68b65022e6098095d9784128d5e
PERSEO2000 <119625141+PERSEO2000@users.noreply.github.com>
2022-12-10 17:04:19 -0600

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,10 +1,6 @@
-
-var overlay = document.querySelector("#overlay");
-var info = document.querySelector("#info");
-var cerrar = document.getElementById("ce");
-
 var link1 = document.getElementById("p1");
 
+var descarga = document.getElementById("link");
 
 
 
@@ -17,13 +13,22 @@ var link1 = document.getElementById("p1");
 
 
 link1.addEventListener("click", function(){
- // overlay.style.display = "block";
+ descarga.style.display = "block";
   //info.style.display = "block";
   
   
  // window.location.href="https://www.mediafire.com/file/aq3z38ieizl3gpw/1231241x.rar/file";
 });
 
+
+
+var link2 = document.getElementById("p2");
+var descarga2 = document.getElementById("link2");
+
+
+link2.addEventListener("click",function(){
+  descarga2.style.display="block";
+})
 
 
 
