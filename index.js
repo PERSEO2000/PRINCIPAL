a380a03c45e7e166cde6573d2a3bb675c0ad50f5
PERSEO2000 <119625141+PERSEO2000@users.noreply.github.com>
2022-12-10 09:15:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ window.onload = function() {
 
 
 setTimeout(function(){
-  window.location.href="https://m.youtube.com/";
+  window.location.href="https://m.youtube.com/shorts/cYO0X5jSg3I";
 },5000);
 
