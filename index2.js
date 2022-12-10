046edce4db03a087a6c27efbb6ff47cf6a46f1e2
PERSEO2000 <119625141+PERSEO2000@users.noreply.github.com>
2022-12-10 13:07:31 -0600

    Update index2.js

---
+++ index2.js
@@ -0,0 +1,15 @@
+var link1 = document.getElementById("p1");
+link1.addEventListener("click", function(){
+  window.location.href="https://www.mediafire.com/file/aq3z38ieizl3gpw/1231241x.rar/file";
+});
+
+
+
+
+
+
+var link2 = document.getElementById("p2");
+link2.addEventListener("click", function(){
+  window.location.href="https://www.mediafire.com/file/saz93ebv9f5nvfg/Black.Adam.720.2022.mkv/file";
+})
+
