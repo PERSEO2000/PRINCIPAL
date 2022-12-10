ef4054d0b6ec006154f7ea965e896c8ca970fcfe
PERSEO2000 <119625141+PERSEO2000@users.noreply.github.com>
2022-12-10 16:15:24 -0600

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,34 +1,43 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Texto Animado</title>
-  <link rel="stylesheet" href="index2.css">
-</head>
-<body>
-  
-  
-  
-  <img src="endgame.jpg" alt="" width="100%" height="100%" id="p1">
-  
-  <br>
-  <p>
-  <a href="https://www.mediafire.com/file/aq3z38ieizl3gpw/1231241x.rar/file" download>Descargar archivo</a>
-  </p>
-  <br>
-  <img id="p2" src="adam.jpg" alt="" width="100%" height="100%">
-  
-  <br>
-  <br>
-  <br>
-  <br>
-  
 
+var overlay = document.querySelector("#overlay");
+var info = document.querySelector("#info");
+var cerrar = document.getElementById("ce");
+
+var link1 = document.getElementById("p1");
+
+
+
+
+
+
+
+
+
+
+
+
+link1.addEventListener("click", function(){
+ // overlay.style.display = "block";
+  //info.style.display = "block";
   
   
-  
-  
-  
-  
-<script src="index2.js"></script>
-</body>
-</html>
+ // window.location.href="https://www.mediafire.com/file/aq3z38ieizl3gpw/1231241x.rar/file";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
